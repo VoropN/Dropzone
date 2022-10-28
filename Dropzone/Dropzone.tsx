3486a1a578e6e9255219d472922f503e47b865d8
VoropN <49345001+VoropN@users.noreply.github.com>
2022-10-28 21:20:53 +0300

    add onDragOver; onDrop

--- Dropzone/Dropzone.tsx
+++ Dropzone/Dropzone.tsx
@@ -1,11 +1,19 @@
+import { memo, DragEventHandler } from 'react';
 import style from './style.module.scss';
 
-export const Dropzone = () => {
+export const Dropzone = memo(() => {
+  const onDragOver = (event: DragEventHandler<HTMLLabelElement>) => {
+    event.preventDefault();
+  };
+  const onDrop = (event: DragEventHandler<HTMLLabelElement>) => {
+    event.preventDefault();
+  };
+
   return (
     <div className={style.container}>
-      <label className={style.dropzone}>
+      <label className={style.dropzone} onDrop={onDrop} onDragOver={onDragOver}>
         <input multiple type="file" tabIndex={-1} className={style.fileInput} />
       </label>
     </div>
   );
-};
+});
