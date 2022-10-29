21c035a3f10acb0da143ba6c8fcde0a28d6441c1
VoropN <49345001+VoropN@users.noreply.github.com>
2022-10-29 16:12:03 -0400

    update FileSet

--- App.tsx
+++ App.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import { Dropzone } from './src/Dropzone';
-import { FileUploader } from './src/FileUploader';
 import { IFileSet } from './src/types';
-import { FileSet } from './FileSet';
+import { FileSet } from './src/FileSet';
 
 import './style.scss';
 
--- src/FileSet/FileSet.tsx
+++ src/FileSet/FileSet.tsx
@@ -3,6 +3,7 @@ import { FileUploader} from '../FileUploader';
 import {IFileSet} from '../types'
 
 const FileSet = React.memo(({ fileSet }: { fileSet: IFileSet }) => {
+  console.log(fileSet)
   return fileSet.map((file, idx) => (
     <FileUploader file={file} key={`${file.name}-idx-${idx}`} />
   );
