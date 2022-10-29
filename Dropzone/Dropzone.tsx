11b647231125ff16c5da59deff7d15147b3d7e50
VoropN <49345001+VoropN@users.noreply.github.com>
2022-10-29 04:27:57 +0300

    add onUploadFile

--- Dropzone/Dropzone.tsx
+++ Dropzone/Dropzone.tsx
@@ -1,11 +1,12 @@
-import type { DragEvent } from 'react';
 import { memo, useState } from 'react';
+import type { DragEvent, ChangeEvent } from 'react';
 import { FiUpload } from 'react-icons/fi';
 import cn from 'classnames';
 import style from './style.module.scss';
 
 export const Dropzone = memo(() => {
   const [isDropzoneHovered, setIsDropzoneHovered] = useState(false);
+  const [files, setFiles] = useState<null | File[]>(null);
   const onHover = ({
     isHovered,
     event,
@@ -29,6 +30,21 @@ export const Dropzone = memo(() => {
   };
   const onDrop = (event: DragEvent<HTMLLabelElement>) => {
     onHover({ isHovered: false, event });
+    const { files } = event.dataTransfer;
+    onLoadFiles(files);
+  };
+  const onLoadFiles = (files: FileList | null) => {
+    if (files?.length) {
+      const fileList = [];
+      for (const idx in files) {
+        fileList.push(files[idx]);
+      }
+      setFiles(fileList);
+    }
+  };
+  const onUploadFile = (event: ChangeEvent<HTMLInputElement>) => {
+    const { files } = event.target;
+    onLoadFiles(files);
   };
 
   return (
@@ -43,7 +59,16 @@ export const Dropzone = memo(() => {
         onDragEnter={onDragEnter}
       >
         <FiUpload className={style.uploadIcon} />
-        <input multiple type="file" tabIndex={-1} className={style.fileInput} />
+        {files?.map((file) => (
+          <div>{file.name}</div>
+        ))}
+        <input
+          multiple
+          type="file"
+          tabIndex={-1}
+          className={style.fileInput}
+          onChange={onUploadFile}
+        />
       </label>
     </div>
   );
